215792584d2007a1d9eae786cee148f3e7c2d084
Frank Rodriguez <lokenator73@gmail.com>
2017-11-22 22:10:05 -0500

    Added Font Awesome and Bower to PC

--- obj/Debug/netcoreapp2.0/D_of_C_Blog.AssemblyInfo.cs
+++ obj/Debug/netcoreapp2.0/D_of_C_Blog.AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("D_of_C_Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("D_of_C_Blog")]
